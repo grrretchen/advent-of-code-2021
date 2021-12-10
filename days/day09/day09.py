767eb0d8883f3edf6c2469ee95e616e984a2ee94
Peter Keppel <pckeppel@gmail.com>
2021-12-09 20:31:11 -0600

    day9 part1 solution

--- days/day09/day09.py
+++ days/day09/day09.py
@@ -9,41 +9,51 @@ class HeightMap:
 		self.dataset = dataset
 		self.np = np.array(dataset)
 
-		self.growSet()
+		self._expand()
 		self.depth = np.zeros(self.np.shape, dtype=int)
+		self.flood()
+		self.part1()
+		
+	def part1(self):
+		r1 = np.where(self.depth==-4, self.np, 0)
+		r2 = np.where(self.depth==-4, 1, 0)
+		r3 = np.sum([r1+r2])
+		return(r3)
+		
+	def flood(self):
 
-		print(self.np)
-		print(self.depth)
-		self.compare()
+		w = np.roll(self.np.copy(),-1,axis=0)
+		a = np.roll(self.np.copy(),-1,axis=1)
+		s = np.roll(self.np.copy(),1,axis=0)
+		d = np.roll(self.np.copy(),1,axis=1)
+		
+		for e in [w,a,s,d]:
+			r = self.np.copy() - e
+			r = np.where((r)<0, -1, (r))
+			r = np.where((r)>0, 1, (r))
+			self.depth += r
+		
 
-	def compare(self):
-
-		home = self.np.copy()
-
-		left = np.roll(self.np.copy(),1,axis=1)
-		print(left)
-
-		alpha = home-left
-		alpha = np.where((alpha)<0, -1, (alpha))
-		alpha = np.where((alpha)>0, 1, (alpha))
-		self.depth += alpha
-		print(self.depth)
-		# print(result)
-		# print(np.where((self.np - left)<0,-1))
-
-
-		# move everything left
-		# self.depth = self.np - mask
-		# print(self.depth)
-
-	def growSet(self):
+	def _expand(self):
 		# expand the width by duplicating the first and last columns
-		self.np = np.append(self.np[[0],:], self.np, 0)
-		self.np = np.append(self.np, self.np[[-1],:], 0)
+		self.np = np.append(self.np[[0],:]+1, self.np, 0)
+		self.np = np.append(self.np, self.np[[-1],:]+1, 0)
 
 		# expand the height by duplicating the first and last rows
-		self.np = np.append(self.np[:,[0]], self.np, 1)
-		self.np = np.append(self.np, self.np[:,[-1]], 1)
+		self.np = np.append(self.np[:,[0]]+1, self.np, 1)
+		self.np = np.append(self.np, self.np[:,[-1]]+1, 1)
+		
+		
+	def _shrink(self):
+		for ar in [self.np, self.depth]:
+			ar = np.delete(ar, -1, axis=0)
+			ar = np.delete(ar, -1, axis=1)
+			ar = np.delete(ar, 0, axis=0)
+			ar = np.delete(ar, 0, axis=1)
+			print(ar)
+		
+		print(self.np)
+		print(self.depth)
 		
 
 # --------------------------------------------------------------------------	
@@ -64,7 +74,6 @@ def fetch(fpath):
 		for line in infile:
 			dataset.append([int(i) for i in line.strip()])
 				
-	print(dataset)
 	return dataset
 
 
@@ -73,7 +82,7 @@ def fetch(fpath):
 def solve(dataset):
 	hm = HeightMap(dataset)
 
-	result1 = None
+	result1 = hm.part1()
 	result2 = None
 
 	return (result1, result2)	
@@ -82,8 +91,8 @@ def solve(dataset):
 # --------------------------------------------------------------------------
 # do the main 
 def main():
-	fpath = "./day09-sample.txt" # this is the sample dataset.
-	# fpath = "./day09-data.txt"
+	#fpath = "./day09-sample.txt" # this is the sample dataset.
+	fpath = "./day09-data.txt"
 
 	dataset = fetch(fpath)
 
