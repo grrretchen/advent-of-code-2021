2c2b51c37b86f0493f867a1775378d7a37ceef4b
Peter Keppel <pckeppel@gmail.com>
2021-12-11 08:37:58 -0600

    day 10 part 1 solution

--- days/day10/day10.py
+++ days/day10/day10.py
@@ -1,3 +1,47 @@
+class Doc:
+	def __init__(self, data):
+		self.data = data
+		self.map = {
+			"{" : "}",
+			"[" : "]",
+			"<" : ">",
+			"(" : ")"
+		}
+		self.points = {
+			"}" : 1197,
+			"]" : 57,
+			">" : 25137,
+			")" : 3
+		}
+		self.score = 0
+		
+		
+	def line(self,line):
+		l = ["{", "<", "(", "[" ]
+		r = ["}", ">", ")", "]" ]
+		b = []
+		print("-------------")
+		print(line)
+		
+		for i in range(len(line)):
+			a = line[i]
+			if a in l:
+				b.insert(0, self.map[a])
+				continue
+			elif b[0] == a:
+				b.pop(0)
+			else:
+				print(f"Expected {b[0]}, but found {a} instead. Points: {self.points[a]}")
+				self.score += self.points[a]
+				return	
+		print("incomplete")
+		
+	def part1(self):
+		for line in self.data:
+			self.line(line)
+		
+		return self.score
+
 
 # --------------------------------------------------------------------------	
 # pull the dataset from a file 
@@ -6,7 +50,7 @@ def fetch(fpath):
 	
 	with open(fpath, "r") as infile:
 		for line in infile:
-			dataset.append([int(i) for i in line.strip()])
+			dataset.append(line.strip())
 				
 	return dataset
 
@@ -14,8 +58,8 @@ def fetch(fpath):
 # --------------------------------------------------------------------------	
 # solve the problems.
 def solve(dataset):
-
-	result1 = None
+	doc = Doc(dataset)
+	result1 = doc.part1()
 	result2 = None
 
 	return (result1, result2)	
@@ -24,7 +68,7 @@ def solve(dataset):
 # --------------------------------------------------------------------------
 # do the main 
 def main():
-	# fpath = "./day10-sample.txt" # this is the sample dataset.
+	#fpath = "./day10-sample.txt" # this is the sample dataset.
 	fpath = "./day10-data.txt"
 	dataset = fetch(fpath)
 	
