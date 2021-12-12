384089a739ec61714f6cc57a3280626f107e3468
Peter Keppel <pckeppel@gmail.com>
2021-12-11 19:20:58 -0600

    day 11 seed

---
+++ days/day11/day11.py
@@ -0,0 +1,37 @@
+
+# --------------------------------------------------------------------------	
+# pull the dataset from a file 
+def fetch(fpath):
+	dataset = []
+	
+	with open(fpath, "r") as infile:
+		for line in infile:
+			dataset.append(line.strip())
+				
+	return dataset
+
+
+# --------------------------------------------------------------------------	
+# solve the problems.
+def solve(dataset):
+	
+	result1 = None
+	result2 = None
+
+	return (result1, result2)	
+
+
+# --------------------------------------------------------------------------
+# do the main 
+def main():
+	#fpath = "./day11-sample.txt" # this is the sample dataset.
+	fpath = "./day11-data.txt"
+	dataset = fetch(fpath)
+	
+	r1,r2 = solve(dataset)
+	print(f"Part 1:\t{r1}\r\nPart 2:\t{r2}")	
+
+
+# ==========================================================================
+if __name__ == "__main__" :
+	result = main()
