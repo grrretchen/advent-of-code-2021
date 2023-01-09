5cf956c134e59b5b6f5333e25099d6d46d87b55d
Peter <pckeppel@gmail.com>
2023-01-09 09:55:24 -0600

    Day 02 - Updated by Gretchen

--- days/day02/day02.py
+++ days/day02/day02.py
@@ -1,90 +1,101 @@
-# generate a data set from a sample, or from a file. --------------------------
-def sample(fpath):
-    dataset = []
-
-    with open(fpath, "r") as infile:
-        for line in infile:
-            # some of these lines might be blank, so skip those.
-            try:
-                dataset.append(line)
-            except:
-                continue
-
-    print("Total records: %s"%len(dataset))
-    return dataset
+class Problem:
+  def __init__(self,dataset):
+    self.result1 = None
+    self.result2 = None
+    self.dataset = self.parse(dataset)
 
 
-# parse the dataset into a list -----------------------------------------------
-def parse(feed):
-    # split this into a list of lists
-    alpha = [a.strip().split(" ") for a in feed]
+  # ------------------------------------------------------------------------
+  # parse the dataset into a list -----------------------------------------------
+  def parse(self, data):
+      # split this into a list of lists
+      alpha = [a.strip().split(" ") for a in data]
 
-    # and list-comprehension to convert the second value into an int.
-    beta = [[f[0], int(f[1]) if f[1] is type(int) else f[1]] for f in alpha]
+      # and list-comprehension to convert the second value into an int.
+      beta = [[f[0], int(f[1]) if f[1] is type(int) else f[1]] for f in alpha]
 
-    return beta
+      return beta  
 
+  # ------------------------------------------------------------------------
+  def part1(self):
+    path = {
+      'forward':0,
+      'up':0,
+      'down':0
+      }
 
-# determine the total offset along each vector (up, down, forward, back) ------
-def pilot(path):
-    position = {}
-    for point in path:
-        if len(point) != 2:
-            continue
-        if not point[0] in position:
-            position[point[0]] = 0
-        position[point[0]] += int(point[1])
+    for row in self.dataset:
+      path[row[0]]+= int(row[1])
 
-    return(position)
+    h = path['forward']
+    v = path['down'] - path['up']
 
-
-# find the true h and true v offset, for a final endpoint. --------------------
-def endpoint(delta):
-    for mode in ['up', 'down', 'forward', 'back']:
-        if not mode in delta:
-            delta[mode] = 0
-    
-    v = delta['down'] - delta['up']
-    h = delta['forward'] - delta['back']
-
-    return {"h" : h, "v": v}
+    print("h: %s\t v: %s"%(h,v))
+    self.result1 = h*v
+    return(self.result1)
 
 
-# for part 2, we need to multiply the vertical aim by the horizontal movement. -
-def aim(frameset):
+  # ------------------------------------------------------------------------
+  # for part 2, we need to multiply the vertical aim by the horizontal movement. -
+  def part2(self):
     aim = 0
     h = 0
-    d = 0
-    for point in frameset:
+    v = 0
+    for point in self.dataset:
         if len(point) != 2:
             continue
         if point[0] in ['up', 'down']:
             # the aim is a rolling sum of previous up/down movements.
             aim += int(point[1]) if point[0] == 'down' else int(point[1])*-1
         if point[0] == "forward":
-            d += int(point[1]) * aim
+            v += int(point[1]) * aim
             h += int(point[1])
 
-    return {"h" : h, "v" : d}
+    print("h: %s\t v: %s"%(h,v))
+    self.result2 = h*v
+    return(self.result2)
 
 
-# do the main -----------------------------------------------------------------
+# --------------------------------------------------------------------------
+# pull the dataset from a file 
+def fetch(fpath):
+  dataset = []
+  
+  with open(fpath, "r") as infile:
+    dataset = [line.strip("\n\r") for line in infile.readlines()]
+        
+  return dataset
+
+
+# --------------------------------------------------------------------------  
+# solve the problems.
+def solve(dataset):
+  p = Problem(dataset)
+
+  p.part1()
+  p.part2()
+
+  return (p.result1, p.result2)  
+
+
+# --------------------------------------------------------------------------
+# do the main 
 def main():
-    # fpath = "./day02-sample.txt" # this is the sample dataset.
-    fpath = "./day02-data.txt"
-    dataset = sample(fpath)
-    frameset = parse(dataset)
-    delta = pilot(frameset)
-    pos_p1 = endpoint(delta)
-    part1 = pos_p1["h"] * pos_p1["v"]
-    print(part1)
+  fpath = "./sample.txt" # this is the sample dataset.
+  fpath = "./data.txt"
+  dataset = fetch(fpath)
+  
+  r1,r2 = solve(dataset)
+
+  result = {
+    "Part 1" : r1,
+    "Part 2" : r2
+  }
+
+  return result
 
 
-    pos_p2 = aim(frameset)
-    part2 = pos_p2["h"] * pos_p2["v"]
-    print(part2)
-    # return result
-
-# =============================================================================
+# ==========================================================================
 if __name__ == "__main__" :
-    result = main()
+  result = main()
+  print(result)
